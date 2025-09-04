28ba16da3bb066fd6b4bbf035c08cebe444de273
noegodin <noegodin443@gmail.com>
2025-09-04 18:40:00 +0200

    fix : correct data type

--- src/types/ui.types.ts
+++ src/types/ui.types.ts
@@ -4,6 +4,9 @@ import type { UserSession } from "./session.types.ts";
 export interface StickCounterProps {
   playerName: string;
   sticks: Stick[];
+  roomId?: string;
+  player: "player1" | "player2";
+  onSticksUpdate?: (newSticks: Stick[]) => void;
 }
 
 export interface StickLogProps {
