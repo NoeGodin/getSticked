608dbf7dd8b4308ed0fea8d3b4aa47bbc04d1e58
Noé Godin <noegodin443@gmail.com>
2025-09-05 10:09:23 +0200

    refactor : changing way we handle player so you can have multiple people in the future

--- src/types/room.types.ts
+++ src/types/room.types.ts
@@ -1,16 +1,17 @@
 import type { Stick } from "./stick.types.ts";
 
+export interface Player {
+  id: string;
+  name: string;
+  sticks: Stick[];
+}
+
 export interface Room {
   id?: string; // Firebase document ID
   name: string;
   secretKey: string;
   description?: string;
-  player1Name: string;
-  player2Name: string;
-  batons: {
-    player1: Stick[];
-    player2: Stick[];
-  };
+  players: Player[];
   createdAt: string; // ISO string
   updatedAt?: string; // ISO string
 }
